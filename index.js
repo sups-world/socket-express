fbd599dbacb96ed68cec9d4c5ecd4e0043b5ceda
Suprajna Sharma <suprasubedi11@gmail.com>
2023-12-06 12:45:07 +0545

    render html file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { createServer } from "node:http";
 
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
 const app = express();
 const server = createServer(app);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get("/", (req, res) => {
-  res.send("Hello there");
+  res.sendFile(join(__dirname, "index.html"));
 });
 
 server.listen(3000, () => {
